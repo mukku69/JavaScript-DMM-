9c33adade441a73bacd7c717568e23aa23f930a3
mukku69 <programing0803@gmail.com>
2021-04-18 12:23:26 +0900

    [Start]６章

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,9 +1,8 @@
 $(function(){
-  $('.box1').slideDown(function(){
-   $('.box1').css({
-    'background-color':'#0000ff',
-    'height':'100px',
-    'width':'200px'
-  }).slideUp();
- });
+  $('.box1').mouseover(function(){
+   $('.box1').css({'background-color':'#0000ff'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#FF0000'});
+  });
 });
